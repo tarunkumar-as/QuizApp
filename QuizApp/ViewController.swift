e2bb9f8d9bceacf6ed894039548bc2e8ff8bc2ca
tarunkumar-as <tarunkumar.as@outlook.com>
2019-09-05 17:46:58 -0400

    Initial Commit

---
+++ QuizApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by Tarun Kumar on 9/5/19.
+//  Copyright © 2019 Tarun Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
